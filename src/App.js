6493a17068b4f01edbf45e3201959d7c7c79710c
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-04-22 17:02:50 +0900

    refactor: 코드 설명 추가

--- src/App.js
+++ src/App.js
@@ -8,16 +8,21 @@ import PostListParameter from "./components/post/PostListParameter";
 function App() {
   return (
     <RecoilRoot>
+      <h1>값을 전역으로 관리하는 기능 (카운터)</h1>
       <Counter />
       <Counter />
       <hr />
+      <h1>값을 전역으로 관리하는 기능 (배열)</h1>
       <TodoList />
       <TodoList />
       <hr />
+      <h1>글자 수 세기</h1>
       <CharacterCounter />
       <hr />
+      <h1>데이터 단순 요청</h1>
       <PostListBasic />
       <hr />
+      <h1>데이터 파라미터로 요청</h1>
       <PostListParameter />
       <hr />
     </RecoilRoot>
