64654f5361960374dee405219414a19527fa0d5f
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-05-26 23:56:57 +0900

    fix: snapshot.getPromise으로 버그 수정

--- src/pages/UseRecoilCallbackPage.js
+++ src/pages/UseRecoilCallbackPage.js
@@ -5,18 +5,19 @@ function UseRecoilCallbackPage() {
 
   const sentences = useRecoilValue(postsState);
 
-  const addRandomSentence = useRecoilCallback(({ set }) => async () => {
+  const addRandomSentence = useRecoilCallback(({ snapshot, set }) => async () => {
     const postId = Math.floor(Math.random() * 100) + 1;
     const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`);
     const newSentence = await response.json();
-    set(postsState, [...sentences, newSentence]);
+    const currentArray = await snapshot.getPromise(postsState);
+    set(postsState, [...currentArray, newSentence]);
   }, []);
 
   return (
     <div>
       <button onClick={addRandomSentence}>아무 문장 하나 추가하기</button>
       <div>
-        {sentences.map(sentence => JSON.stringify(sentence))}
+        {sentences.map(sentence => <div key={sentence.id}>{sentence.body}</div>)}
       </div>
     </div>
   );
